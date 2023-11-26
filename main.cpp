e3b34dd4fbe113c26cf7b2f10b6e8e912e8160e3
SashaGarmanov <sagagarm8357@gmail.com>
2023-11-26 11:00:16 +0300

    main function

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int inputPositiveNumberA() {
+}
+
+int inputPositiveNumberB() {
+}
+
+int calculateRectangleArea(int length, int width) {
+}
+
+int calculateRectanglePerimeter(int length, int width) {
+}
+
+int main() {
+    int A, B, C;
+    cout<<"1. Ввести положительное число А"<<endl;
+    cout<<"2. Ввести положительное число B"<<endl;
+    cout<<"3. Найти площадь прямоугольника"<<endl;
+    cout<<"4. Найти периметр прямоугольника"<<endl;
+    
+    do{
+        cin>>C;
+        switch(C){
+            case 1:
+                A = inputPositiveNumberA();
+                break;
+            case 2:
+                B = inputPositiveNumberB();
+                break;
+            case 3:
+                cout<<"Площадь прямоугольника: "<<calculateRectangleArea(A, B)<<endl;
+                break;
+            case 4:
+                cout<<"Периметр прямоугольника: "<<calculateRectanglePerimeter(A, B)<<endl;
+                break;
+        }
+    }while(true);
+
+
+}
